461b0c109ae6efb0cf0ffdf7f5b9d9b27e1bac7f
George van Heerden <george.vanheerden@sovtech.com>
2023-05-04 09:53:19 +0200

    added logs

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs, resolvers } from './schema/index.js';
+console.log('4');
 const port = parseInt(process.env.PORT) || 3000;
 console.log('port: ', port);
+console.log('7');
 const server = new ApolloServer({ typeDefs, resolvers });
+console.log('9');
 const { url } = await startStandaloneServer(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
     listen: { port: port },
 });
+console.log('13');
 console.log(`🚀  Server ready at ${url}`);
--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { typeDefs, resolvers } from './schema/index.js'
-
-interface MyContext {
-  token?: String
-}
+console.log('4')
 const port = parseInt(process.env.PORT) || 3000
 console.log('port: ', port)
-
-const server = new ApolloServer<MyContext>({ typeDefs, resolvers })
-
+console.log('7')
+const server = new ApolloServer({ typeDefs, resolvers })
+console.log('9')
 const { url } = await startStandaloneServer(server, {
-  context: async ({ req }) => ({ token: req.headers.token }),
   listen: { port: port },
 })
+console.log('13')
 console.log(`🚀  Server ready at ${url}`)
