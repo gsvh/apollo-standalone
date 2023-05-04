0c883dbec9a21b46b5449777817f67fa424be7b2
George van Heerden <george.vanheerden@sovtech.com>
2023-05-04 10:02:20 +0200

    converted to express

--- index.ts
+++ index.ts
@@ -1,14 +1,46 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import { typeDefs, resolvers } from './schema/index.js'
-console.log('4')
-const port = parseInt(process.env.PORT) || 3000
-console.log('port: ', port)
-console.log('7')
-const server = new ApolloServer({ typeDefs, resolvers })
-console.log('9')
-const { url } = await startStandaloneServer(server, {
-  listen: { port: port },
+
+interface MyContext {
+  token?: string
+}
+
+// Required logic for integrating with Express
+const app = express()
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app)
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
-console.log('13')
-console.log(`🚀  Server ready at ${url}`)
+// Ensure we wait for our server to start
+await server.start()
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors<cors.CorsRequest>(),
+  bodyParser.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+)
+
+// Modified server startup
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+console.log(`🚀 Server ready at http://localhost:4000/`)
